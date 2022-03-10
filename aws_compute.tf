a042d2c63621c4d87e760b183195d524c6efa812
jlunde-cisco <75453543+jlunde-cisco@users.noreply.github.com>
2022-03-10 08:57:20 -0600

    Adding files

---
+++ aws_compute.tf
@@ -0,0 +1,86 @@
+# create-instance.tf
+ 
+resource "aws_instance" "db1" {
+  ami                         = var.instance_ami
+  availability_zone           = "${var.aws_region}${var.aws_region_az}"
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.subnet.id
+  key_name                    = var.key_pair
+ 
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    volume_size           = var.root_device_size
+    volume_type           = var.root_device_type
+  }
+ 
+  tags = {
+    "Owner"               = var.owner
+    "Name"                = "Siwapp_db1"
+    "KeepInstanceRunning" = "false"
+    "ansible_group"	  = "db"
+  }
+}
+
+resource "aws_instance" "db2" {
+  ami                         = var.instance_ami
+  availability_zone           = "${var.aws_region}${var.aws_region_az}"
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.subnet.id
+  key_name                    = var.key_pair
+ 
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    volume_size           = var.root_device_size
+    volume_type           = var.root_device_type
+  }
+ 
+  tags = {
+    "Owner"               = var.owner
+    "Name"                = "Siwapp_db2"
+    "KeepInstanceRunning" = "false"
+    "ansible_group"       = "db"
+  }
+}
+
+resource "aws_instance" "db3" {
+  ami                         = var.instance_ami
+  availability_zone           = "${var.aws_region}${var.aws_region_az}"
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.subnet.id
+  key_name                    = var.key_pair
+ 
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    volume_size           = var.root_device_size
+    volume_type           = var.root_device_type
+  }
+ 
+  tags = {
+    "Owner"               = var.owner
+    "Name"                = "Siwapp_db3"
+    "KeepInstanceRunning" = "false"
+    "ansible_group"       = "db"
+  }
+  provisioner "remote-exec" {
+    inline =  ["echo Done!"]
+
+    connection {
+      host        = self.public_ip
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file(var.pvt_key)
+    }
+  }
+   provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user -i '${self.public_ip},' --private-key ${var.pvt_key} ansible/site.yml"
+  }
+}
---
+++ create-sg.tf
@@ -0,0 +1,42 @@
+# create-sg.tf
+ 
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+ 
+resource "aws_security_group" "sg" {
+  name        = "$terraform-ingress-sg"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+ 
+  ingress = [{
+    description      = "So insecure"
+    protocol         = var.sg_ingress_proto
+    from_port        = var.sg_ingress_all
+    to_port          = var.sg_ingress_all
+    cidr_blocks      = [var.sg_all_cidr_block]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+ 
+  }]
+ 
+  egress = [{
+    description      = "All traffic"
+    protocol         = var.sg_egress_proto
+    from_port        = var.sg_egress_all
+    to_port          = var.sg_egress_all
+    cidr_blocks      = [var.sg_all_cidr_block]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+ 
+  }]
+ 
+  tags = {
+    "Owner" = var.owner
+    "Name"  = "terraform-sg"
+  }
+}
