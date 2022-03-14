6f556bca98b08cb5aca241e9a7354c2bca8b27b2
jlunde-cisco <75453543+jlunde-cisco@users.noreply.github.com>
2022-03-14 08:22:02 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -114,7 +114,7 @@ variable "rt_cidr_block" {
 variable "instance_ami" {
   description = "ID of the AMI used"
   type        = string
-  default     = "ami-011b3ccf1bd6db744"
+  default     = "ami-000db10762d0c4c05"
 }
  
 variable "instance_type" {
