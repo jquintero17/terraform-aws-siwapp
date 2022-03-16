6d6bd87efe250d75987a1cd17bf87b5d11c29725
jlunde-cisco <75453543+jlunde-cisco@users.noreply.github.com>
2022-03-16 13:32:53 -0500

    Update create-vpn-attachment.tf

--- create-vpn-attachment.tf
+++ create-vpn-attachment.tf
@@ -1,9 +1,9 @@
 resource "aws_vpn_gateway_attachment" "vpn_attachment" {
   vpc_id         = aws_vpc.vpc.id
-  vpn_gateway_id = vgw-0774fee884724891a
+  vpn_gateway_id = "vgw-0774fee884724891a"
 }
 
 resource "aws_vpn_gateway_route_propagation" "oxyroute" {
-  vpn_gateway_id = aws_vpn_gateway.vpn.id
+  vpn_gateway_id = "vgw-0774fee884724891a"
   route_table_id = aws_route_table.rt.id
 }
