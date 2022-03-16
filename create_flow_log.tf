a24363557929666e0433066a9f98e774bfd7e43d
jlunde-cisco <75453543+jlunde-cisco@users.noreply.github.com>
2022-03-16 13:44:07 -0500

    Create create_flow_log.tf

---
+++ create_flow_log.tf
@@ -0,0 +1,8 @@
+resource "aws_flow_log" "secureworkload" {
+  log_destination      = "arn:aws:s3:::us-west-2-csw-flowlogs"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.vpc.id
+  log_format           = "${account-id} ${action} ${az-id} ${bytes} ${dstaddr} ${dstport} ${end} ${flow-direction} ${instance-id} ${interface-id} ${log-status} ${packets} ${pkt-dst-aws-service} ${pkt-dstaddr} ${pkt-src-aws-service} ${pkt-srcaddr} ${protocol} ${region} ${srcaddr} ${srcport} ${start} ${sublocation-id} ${sublocation-type} ${subnet-id} ${tcp-flags} ${traffic-path} ${type} ${version} ${vpc-id}"
+}
+
